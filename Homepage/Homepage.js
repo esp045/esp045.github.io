cf5b519bc07b5cdf7316c7059f4038dc4344bcbe
esp045 <37131365+esp045@users.noreply.github.com>
2018-06-13 22:08:17 +0900

    Update Homepage.js

--- Homepage/Homepage.js
+++ Homepage/Homepage.js
@@ -36,3 +36,17 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i}; // 숫자가 10보다 작을 경우 앞에 0을 붙여줌
     return i;
 }
+
+/*onload 중복*/
+      function addLoadEvent(func){
+            var oldonload = window.onload;
+            if(typeof window.onload != 'function'){
+                window.onload = func;
+            }else{
+                window.onload = function(){
+                    oldonload();
+                    func();
+                };
+            }
+
+        }
